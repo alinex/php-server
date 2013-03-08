2e2a7da0a7ae8f398b8c8194d01b3db8285b15fd
alex <alex@alex-R505>
2013-03-08 21:38:54 +0100

    added third party library as fallback to gettext

--- source/Alinex/Util/I18n.php
+++ source/Alinex/Util/I18n.php
@@ -11,20 +11,48 @@
 
 namespace Alinex\Util;
 
+use Alinex\Util\String;
+use Alinex\Storage\Registry;
+use Alinex\Template;
+
 /**
  * Internationalization and translation helper methods.
  */
 class I18n
 {
     /**
-     * Initialise the internationalization system.
-     * 
-     * This will include the tr() and trn() functions to be used.
+     * Name in the session to store selected locale
+     * @session
      */
-    static function init()
-    {
-        require_once 'tr.php';
-    }
+    const SESSION_LOCALE = 'alinex.i18n.locale';
+
+    /**
+     * Name in the registry to store possible locales.
+     * If this entry is found only the locales listed here and included in the
+     * system are possible to use.
+     * @registry
+     */
+    const REGISTRY_LOCALES = 'i18n.locales';
+
+    /**
+     * List of languages with popular country codes.
+     * This will be used for extending the locales to it's possible full setting
+     * to a find a corresponding system local for installed languages.
+     * @var array
+     */
+    private static $_languagesToCountry = array(
+        'en' => array('US', 'GB', 'AU'),
+        'de' => array('AT','CH')
+    );
+
+    /**
+     * Flag defining if the system will emulate gettext.
+     * If this is set to true the gettext functions will be emulated using
+     * php methods which directly read from the *.mo files.
+     * If set to NULL the translation system has not been initialized correctly.
+     * @var bool
+     */
+    private static $_emulation = null;
 
     /**
      * Only for testing.
@@ -36,159 +64,99 @@ class I18n
     }
 
     /**
-     * Set the domain from the calling class.
+     * Specify if gettext emulation should be used.
      *
-     * The base namespace will be used as domain. It will be set if not
-     * already done.
-     *
-     * @note This will be called from tr() and trn() automatically, so there
-     * is no need to invoke this directly.
-     *
-     * @param string $namespace namespace of calling class
-     * @throws \RuntimeException if given domain was not set
+     * If nothing specified gettext will be used if the extension is installed.
+     * Only if the installed extension should not be used this method can switch
+     * on the emulation.
+     * @param bool $flag true to use emulation althouight gettext is installed
+     * @return bool flag if emulation is active or not
      */
-    public static function setDomain($namespace)
+    static function emulateGettext($flag)
     {
-        if (!function_exists('gettext'))
-            return;
-        // get top namespace
-        list($domain) = explode('\\', $namespace, 2);
-        if (textdomain(null) == $domain)
-            return;
+        assert(is_bool($flag));
 
-        // closing slash for windows neccessary
-        if (!bindtextdomain($domain, __DIR__.'/../../../tr/'))
-            throw new \Exception('Could not bind translation directory under /tr');
-        // set the dopmain
-        if (!textdomain($domain) == $domain)
-            throw new \Exception('Could not set translation domain to '.$domain.' under /tr');
-        // set codeset to use UTF-8 (optional)
-        bind_textdomain_codeset($domain, "UTF-8");
+        return self::setEmulation(!function_exists('gettext') || $flag == true);
+    }
+
+    private static function setEmulation($flag)
+    {
+        self::$_emulation = $flag;
+        if ($flag) {
+            require_once __DIR__.'/../../vnd/php-gettext/streams.php';
+            require_once __DIR__.'/../../vnd/php-gettext/gettext.php';
+        }
+        return $flag;
+    }
+    /**
+     * Initialise the internationalization system.
+     *
+     * This will include the tr() and trn() functions to be used. If not called
+     * explicitly it will be called before the first use. Althought it is better
+     * behaviour to call this because it will also set the locale for some php
+     * internal functions.
+     */
+    static function init()
+    {
+        require_once 'tr.php';
+        // check for emulation of gettext
+        if (!function_exists('gettext'))
+            self::setEmulation(true);
+        // set the locale
+        self::setLocale();
     }
 
     /**
-     * Current locale
-     * @var string
-     */
-    private static $_locale = null;
-    
-    /**
-     * Set the system local.
+     * Set the system locale.
      *
-     * @param string $locale user provided locale
+     * If no locale given it will be autodetected.
+     *
+     * @param string|array $locales user provided locale
      * @return the local which is set
      */
-    public static function setLocale($locale)
+    public static function setLocale($locales = null)
     {
-        // set system setting
-        if ($locale)
-            $locale = setlocale(LC_ALL, $locale);
-        // try extended search for possible locale
-        if (!$locale)
-            $locale = setlocale(LC_ALL, self::extendLocales($locale));
-        // locale could not be set
-        if (!$locale)
-            return false;
-        self::$_locale = $locale;
+        if (is_string($locales))
+            $locales = array($locales);
+        // check if already selected in session
+        if (!isset($locales) && isset($_SESSION[self::SESSION_LOCALE]))
+            $set = setlocale(LC_ALL, $_SESSION[self::SESSION_LOCALE]);
+        // if nothing given auto detect the local
+        if (!$set && !isset($locales))
+            return self::setLocale(self::localeDetect());
+        // find optimal locale
+        if (!$set) {
+            // generalize and filter for alinex
+            $locales = self::localeFilter(self::localeExtend($locales));
+            // try extended list
+            if ($locales)
+                $set = setlocale(LC_ALL, $locales);
+            // try to use earlier set version
+            if (isset($_SESSION[self::SESSION_LOCALE]))
+                $set = setlocale(LC_ALL, $_SESSION[self::SESSION_LOCALE]);
+            // locale could not be set
+            if (!$set)
+                return false;
+            // store in session
+            $_SESSION[self::SESSION_LOCALE] = $set;
+        }
         // set the locale in environment, too
-        putenv('LC_ALL='.$locale);
-        putenv('LANG='.$locale);
+        putenv('LC_ALL='.$set);
+        putenv('LANG='.$set);
+        putenv('LANGUAGE='.$set);
         // return used locale
-        return $locale;
-    }
-
-    /**
-     * Extend the list of locales by getting alternatives.
-     *
-     * This is done in 3 steps:
-     * <ol>
-     * <li>add the locale with utf8 encoding
-     * <li>add specializations (extended with country...)
-     * <li>add generalization (only language code)
-     * </ol>
-     *
-     * The modifier is not added on the possible tests.
-     * 
-     * @param string $locale locale to find alternatives
-     * @param bool $onlyExtend this is used in continued calls to not 
-     * generalize again
-     * @return extended list of locales
-     */
-    private static function extendLocales($locale, $onlyExtend = false)
-    {
-        assert(is_string($locale));
-        
-        $extended = array();
-        // analyse locale
-        $part = array();
-        if (!preg_match("/^(?P<lang>[a-z]{2,3})"              // language code
-                       ."(?:_(?P<country>[A-Z]{2}))?"           // country code
-                       ."(?:\.(?P<charset>[-A-Za-z0-9_]+))?"    // charset
-                       ."(?:@(?P<modifier>[-A-Za-z0-9_]+))?$/",  // @ modifier
-                       $locale, $part))
-            return array(); // no POSIX style language
-        // add alternatives
-        if (isset($part['modifier'])) {
-            if ($country) {
-                if ($charset)
-                    array_push($extended, "${lang}_$country.$charset@$modifier");
-                array_push($extended, "${lang}_$country@$modifier");
-            } elseif ($charset)
-                array_push($extended, "${lang}.$charset@$modifier");
-            array_push($extended, "$lang@$modifier");
-        }
-        if ($country) {
-            if ($charset)
-                array_push($extended, "${lang}_$country.$charset");
-            array_push($extended, "${lang}_$country");
-        } elseif ($charset)
-            array_push($extended, "${lang}.$charset");
-        array_push($extended, $lang);
-        // return result
-        return $extended;
-    }
-    
-
-    
-    
-    /**
-     * Get specific locals to be used.
-     *
-     * If an additional locale is given this will be added on top of the
-     * wishlist for the local setting.
-     *
-     * @param string $locale user provided locale
-     * @return array list of possible locales
-     */
-    private static function getLocales($locale = null)
-    {
-        // analyze browser and store settings in session
-        $sname = self::PREFIX.'locales';
-        if (!isset($_SESSION[$sname]))
-            $_SESSION[$sname] = self::detectLocales();
-        // add specified local topmost
-        if (isset($locale)) {
-            // remove old settings of same entry
-            array_diff($_SESSION[$sname], array($locale));
-#            $_SESSION[$sname] = \array_merge(self::extendLocales(array($locale)), $_SESSION[$sname]);
-            $list = self::extendLocales(array($locale));
-            foreach($_SESSION[$sname] as $l)
-                if (!in_array($l, $list))
-                    $list[] = $l;
-            $_SESSION[$sname] = $list;
-        }
-        return $_SESSION[$sname];
+        return $set;
     }
 
     /**
      * Detect the accepted locales from client information.
      *
      * First the browser settings from HTTP header will be used and
-     * alternatively the client's first level domain.
+     * alternatively the client's top level domain.
      *
      * @return array list of possible locales
      */
-    private static function detectLocales()
+    private static function localeDetect()
     {
         $locales = array();
         // find user accept locales
@@ -217,17 +185,237 @@ class I18n
         }
         // find user locale by domain name
         if (isset($_SERVER['REMOTE_HOST'])) {
-            $locales[] = strtolower(
-                end($h = explode('.', $_SERVER['REMOTE_HOST']))
-            );
+            $locales[] = strtolower(end(explode('.',$_SERVER['REMOTE_HOST'])));
         }
-
         return self::extendLocales($locales);
     }
 
-    private static $_defaultLocales = array(
-        'en_US', 'en_GB','de_DE'
-    );
+    /**
+     * Extend the given locale names.
+     *
+     * This is done in two different steps:
+     * - generalization list upper locales till only language code
+     * - specialization to country code and charset
+     *
+     * @param array $locales user provided locales
+     * @return array list of possible locales
+     */
+    private static function localeExtend($locales)
+    {
+        $lang = array(); // simplest entries using ll
+        $cont = array(); // advanced entries using ll_CC
+        $cset = array(); // full entries using ll_CC.charset
+        foreach ($locales as $check) {
+            $part = null;
+            if (!preg_match(
+                "/^(?P<lang>[a-z]{2,3})"                 // language code
+                ."(?:_(?P<country>[A-Z]{2}))?"           // country code
+                ."(?:\.(?P<charset>[-A-Za-z0-9_]+))?"    // charset
+                ."(?:@(?P<modifier>[-A-Za-z0-9_]+))?$/", // @ modifier
+                $check, $part
+            )) continue; // no POSIX style language
+            if (isset($part['country'])) {
+                if (isset($part['charset'])) {
+                    $cset[] = $check;
+                    // specialize
+                    if ($part['charset'] != 'utf8')
+                        $cset[] = $part['lang'].'_'.$part['country'].'.utf8';
+                    // generalize
+                    $cont[] = $part['lang'].'_'.$part['country'];
+                    $lang[] = $part['lang'];
+                } else {
+                    $cont[] = $check;
+                    // specialize
+                    $cset[] = $part['lang'].'_'.$part['country'].'.utf8';
+                    // generalize
+                    $lang[] = $part['lang'];
+                }
+            } else {
+                $lang[] = $check;
+                // specialize
+                $cset[] = $part['lang'].'_'.strtoupper($part['lang']).'.utf8';
+                $cont[] = $part['lang'].'_'.strtoupper($part['lang']);
+                // extend by well known country codes
+                if (isset(self::$_languagesToCountry[$part['lang']])) {
+                    foreach (self::$_languagesToCountry[$part['lang']] as $c) {
+                        $cset[] = $part['lang'].'_'.$c.'.utf8';
+                        $cont[] = $part['lang'].'_'.$c;
+                    }
+                }
+            }
+        }
+        return array_merge($cset, $cont, $lang);
+    }
 
+    /**
+     * Filter out all locales not supported by alinex.
+     *
+     * @param array $locales user provided locales
+     * @return array list of possible locales
+     */
+    private static function localeFilter($locales)
+    {
+        foreach (self::localesAlinex() as $check)
+            if (String::startsWith($check, $locales))
+                return true;
+        return false;
+    }
+
+    /**
+     * Get a list of all locales supported by alinex.
+     *
+     * This is done checking the translation files and the registry. Therefore
+     * it is possible to prevent installed locales from usage.
+     *
+     * @return array list of locales
+     */
+    private static function localesAlinex()
+    {
+        // get from files
+        $locales = scandir(__DIR__.'/../../tr');
+        $registry = Registry::getInstance();
+        if ($registry->has(self::REGISTRY_LOCALES)) {
+            // filter with registry setting
+            $locales = array_intersect(
+                $locales,
+                $registry->get(self::REGISTRY_LOCALES)
+            );
+        }
+        return locales;
+    }
+
+    /**
+     * Gettext translate function
+     *
+     * This function uses the gettext library which should be enabled in PHP. If not
+     * installed, no translations will be available and the original text will be
+     * shown.
+     *
+     * @param string $namespace namespace of caller (use __NAMESPACE__)
+     * @param string $msgid text which should be translated
+     * @param array $params params with vars which should be replaced
+     * (enclosed with {})
+     *
+     * @return   string   translated text
+     */
+    static function tr($namespace, $msgid, array $params = array())
+    {
+        // domain has to be set
+        assert(is_string($namespace));
+        // only strings may be translated
+        assert(is_string($msgid));
+
+        self::setDomain($namespace);
+        // get the message
+        if (self::$_emulation)
+            $trans = isset(self::$_reader)
+                // emulated gettext
+                ? self::$_reader->translate($msgid)
+                // fallback keep english
+                : $msgid;
+        else
+            // call native php function
+            $trans = gettext($msgid); // call native php function
+        // replace variables and return
+        return Template\Simple::run($trans, $params);
+    }
+
+    /**
+     * Gettext translate function (with plural)
+     *
+     * This function uses the gettext library which should be enabled in PHP. If not
+     * installed, no translations will be available and the original text will be
+     * shown.
+     *
+     * @param string $namespace namespace of caller (use __NAMESPACE__)
+     * @param string $msgSingular singular text which should be translated
+     * @param string $msgPlural plural text which should be translated
+     * @param integer $num number to decide between singular and plural
+     * @param array $params params with vars which should be replaced
+     * (enclosed with {})
+     *
+     * @return   string   translated text
+     */
+    static function trn(
+        $namespace, $msgSingular, $msgPlural, $num, array $params = array()
+    )
+    {
+        // domain has to be set
+        assert(is_string($namespace));
+        // only strings may be translated
+        assert(is_string($msgSingular));
+        // only strings may be translated
+        assert(is_string($msgPlural));
+        // have to be a positive integer
+        assert(is_int($num) && $num >= 0);
+
+        self::setDomain($namespace);
+        if (self::$_emulation)
+            $trans = isset(self::$_reader)
+                // emulated gettext
+                ? self::$_reader->ngettext($msgSingular, $msgPlural, $num)
+                // fallback keep english
+                : self::$_reader->translate($msgid);
+        else
+            // call native php function
+            $trans = ngettext($msgSingular, $msgSingular, $num);
+        // replace variables and return
+        return Template\Simple::run($trans, $params);
+    }
+
+    private $_domain = null;
+    private $_reader = null;
+
+    /**
+     * Set the domain from the calling class.
+     *
+     * The base namespace will be used as domain. It will be set if not
+     * already done.
+     *
+     * @note This will be called from tr() and trn() automatically, so there
+     * is no need to invoke this directly.
+     *
+     * @param string $namespace namespace of calling class
+     * @return string domain name
+     * @throws \Exception if given domain was not set
+     */
+    private static function setDomain($namespace)
+    {
+        // check for initialization
+        if (!isset(self::$_emulation))
+            self::init();
+        // get top namespace
+        list($domain) = explode('\\', $namespace, 2);
+        // check if gettext will be used
+        if (self::$_emulation) {
+            if ($domain == self::$_domain)
+                return;
+            $locale = setlocale(0);
+            $file = __DIR__.'/../../../tr/'.$locale.'/'.$domain.'.mo';
+            if (!file_exists($file)) {
+                list($locale) = explode('.', $locale, 2);
+                $file = __DIR__.'/../../../tr/'.$locale.'/'.$domain.'.mo';
+                if (!file_exists($file)) {
+                    list($locale) = explode('_', $locale, 2);
+                    $file = __DIR__.'/../../../tr/'.$locale.'/'.$domain.'.mo';
+                    if (!file_exists($file))
+                        throw new \Exception('Could not bind translation directory under /tr');
+                }
+            }
+            self::$_reader = new \FileReader($file);
+            return $domain;
+        }
+        if (textdomain(null) == $domain)
+            return;
+        // closing slash for windows neccessary
+        if (!bindtextdomain($domain, __DIR__.'/../../../tr/'))
+            throw new \Exception('Could not bind translation directory under /tr');
+        // set the dopmain
+        if (!textdomain($domain) == $domain)
+            throw new \Exception('Could not set translation domain to '.$domain.' under /tr');
+        // set codeset to use UTF-8 (optional)
+        bind_textdomain_codeset($domain, "UTF-8");
+        return $domain;
+    }
 
 }
