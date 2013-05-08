5d174a317180d321a6848e0fba48a46786ff2484
Alexander Schilling <info@alinex.de>
2013-05-08 20:27:41 +0200

    name created file after method in case some files got left

--- test/Alinex/Process/File/TouchTest.php
+++ test/Alinex/Process/File/TouchTest.php
@@ -8,21 +8,21 @@ class TouchTest extends \PHPUnit_Framework_TestCase
     
     function testInitial()
     {
-        $pr = new Touch(self::TESTDIR.'file1');
+        $pr = new Touch(self::TESTDIR.'touch1');
         $this->assertTrue($pr->isSuccess());        
-        $this->assertTrue(file_exists(self::TESTDIR.'file1'));
-        $pr = new Touch(self::TESTDIR.'file2');
+        $this->assertTrue(file_exists(self::TESTDIR.'touch1'));
+        $pr = new Touch(self::TESTDIR.'touch2');
         $pr->setTime(time()-6000);
         $this->assertTrue($pr->isSuccess());        
-        $pr = new Touch(self::TESTDIR.'file3');
-        $pr->copyTimeFrom(self::TESTDIR.'file2');
+        $pr = new Touch(self::TESTDIR.'touch3');
+        $pr->copyTimeFrom(self::TESTDIR.'touch2');
         $this->assertTrue($pr->isSuccess());        
         // cleanup
-        $pr = new Rm(self::TESTDIR.'file1');
+        $pr = new Rm(self::TESTDIR.'touch1');
         $pr->exec();
-        $pr = new Rm(self::TESTDIR.'file2');
+        $pr = new Rm(self::TESTDIR.'touch2');
         $pr->exec();
-        $pr = new Rm(self::TESTDIR.'file3');
+        $pr = new Rm(self::TESTDIR.'touch3');
         $pr->exec();        
     }
 
