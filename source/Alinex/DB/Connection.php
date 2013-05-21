f6bce530fa414aab04aa617224c62bd0761e261a
Alexander Schilling <info@alinex.de>
2013-05-21 20:44:17 +0200

    added configurable database connection

--- source/Alinex/DB/Connection.php
+++ source/Alinex/DB/Connection.php
@@ -21,6 +21,13 @@ use Alinex\Dictionary\Engine;
 class Connection
 {
     /**
+     * Registry entry group for db connection settings.
+     * This group will hold named connections with their configuration. The
+     * standard connection is called 'default'.
+     */
+    const REGISTRY_BASE = 'dbconn.';
+    
+    /**
      * Cache group to store SQL results.
      * This will hold the SQL codes which are generated out of DQL syntax.
      */
@@ -54,10 +61,31 @@ class Connection
      * This method returns a Doctrine\DBAL\Connection which wraps the underlying
      * driver connection.
      *
+     * @param string $name name of the configuration
      * @return \Doctrine\DBAL\Connection
      */
-    public static function get()
+    public static function get($name = 'default')
     {
+        // get configuration
+        $registry = \Alinex\Dictionary\Registry::getInstance();
+        if (!isset($registry))
+            throw new \Exception(tr(__NAMESPACE__, 'No registry set up to read configuration from'));
+        // add validators
+        if ($registry->validatorCheck()
+            && !$registry->validatorHas(self::REGISTRY_BASE.$name))
+            $registry->validatorSet(
+                self::REGISTRY_BASE.$name, 
+                __NAMESPACE__.'\Validator::connection',
+                array(
+                    'exclude' => 'Session',
+                    'description' => tr(
+                        __NAMESPACE__,
+                        'Database connection configuration for {name}.',
+                        array('name' => $name)
+                    )
+                )
+            );
+        $connectionConfig = $registry->get(self::REGISTRY_BASE.$name);
         // check for cache
         $cache = self::$_resultCache;
         if (!isset($cache)) {
@@ -70,16 +98,7 @@ class Connection
         $config->setResultCacheImpl($cache);
         if (!PRODUCTIVE)
             $config->setSQLLogger(new SQLLogger());
-        //..
-        $connectionParams = array(
-            'dbname' => 'a3',
-            'user' => 'alinex',
-            'password' => 'test',
-            'host' => 'localhost',
-            'driver' => 'pdo_mysql',
-        );
 
-        return DriverManager::getConnection($connectionParams, $config);
-
+        return DriverManager::getConnection($connectionConfig, $config);
     }
 }
